be8303c2589561caf711721ca125c75737f1af53
emilbratt <emilbratt@gmail.com>
2023-06-01 13:30:43 +0200

    typing

--- self_study/programming/languages/rust/rust_lang_book/2_Programming_a_Guessing_Game/guessing_game/src/main.rs
+++ self_study/programming/languages/rust/rust_lang_book/2_Programming_a_Guessing_Game/guessing_game/src/main.rs
@@ -1,3 +1,22 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    // if we omit the importing on top (first line )
+    // the we could still call the io function using this method below
+    // std::io::stdin()
+    //     .read_line(&mut guess)
+    //     .expect("Failed to read line");
+
+
+    println!("You guessed: {guess}");
 }
