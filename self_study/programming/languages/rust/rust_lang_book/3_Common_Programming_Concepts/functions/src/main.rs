18a546b0bdf7df0eac7aaa5fe57579cf23823c05
emilbratt <emilbratt@gmail.com>
2023-06-05 19:07:47 +0200

    typing

--- self_study/programming/languages/rust/rust_lang_book/3_Common_Programming_Concepts/functions/src/main.rs
+++ self_study/programming/languages/rust/rust_lang_book/3_Common_Programming_Concepts/functions/src/main.rs
@@ -2,8 +2,18 @@ fn main() {
     println!("Hello, world!");
 
     another_function();
+    a_function_with_a_parameter(4);
+    print_labeled_measurement(5, 'h');
 }
 
 fn another_function() {
     println!("Another function.");
+}
+
+fn a_function_with_a_parameter(x: i32) {
+    println!("The value of x is: {x}");
+}
+
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {value}{unit_label}");
 }
