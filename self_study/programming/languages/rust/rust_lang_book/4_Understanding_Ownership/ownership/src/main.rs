b6361edafa33091ec1590ac8ed5df86ae400e7e5
emilbratt <emilbratt@gmail.com>
2023-06-06 13:41:19 +0200

    string literal and String type in Rust

--- self_study/programming/languages/rust/rust_lang_book/4_Understanding_Ownership/ownership/src/main.rs
+++ self_study/programming/languages/rust/rust_lang_book/4_Understanding_Ownership/ownership/src/main.rs
@@ -1,3 +1,32 @@
+// constants can be declared globally
+const X: u32 = 5;
+const Y: &str = "fiiiive";
+
 fn main() {
-    println!("Hello, world!");
+    println!("const X is {X}");
+    println!("const Y is {Y}");
+    immutable_string_literal();
+    mutable_string_type();
+}
+
+fn immutable_string_literal() {
+    // string literal is
+    // ..not mutable
+    // ..hardcoded into compiled binary
+    // ..very performant
+    let immutable_string: &str = "an immutable string";
+    println!("{immutable_string}");
+}
+
+fn mutable_string_type() {
+    // String type is
+    // ..mutable
+    // ..not as performant as a string literal
+    // ..allocated on the heap
+    // ..able to store an amount of text unknown at compile time
+    // ..not hardcoded into compiled binary
+    // ..created with the String::from("..") function
+    let mut mutable_string = String::from("a mutable string");
+    mutable_string.push_str(", can be changed!");
+    println!("{mutable_string}");
 }
