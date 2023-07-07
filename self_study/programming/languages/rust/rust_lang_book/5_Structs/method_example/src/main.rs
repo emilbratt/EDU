59db120a12e280e9d55958fcac8196fe6d01b522
emilbratt <emilbratt@gmail.com>
2023-07-07 23:15:51 +0200

    add example for implementing methods

--- self_study/programming/languages/rust/rust_lang_book/5_Structs/method_example/src/main.rs
+++ self_study/programming/languages/rust/rust_lang_book/5_Structs/method_example/src/main.rs
@@ -7,8 +7,19 @@ struct Rectangle {
 }
 // ..then implementing a method on Rectangle.
 impl Rectangle {
+    // Get area of rectangle.
     fn area(&self) -> u32 {
         self.width * self.height
+    }
+
+    // Rectangle has width.
+    fn has_width(&self) -> bool {
+        self.width > 0
+    }
+
+    // Can the current instances rectangle hold another instances rectangle
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
     }
 }
 
@@ -22,4 +33,28 @@ fn main () {
         "The area of the rectangle is {} square pixels.",
         rect1.area()
     );
+    if rect1.has_width() {
+        println!("The rectangle has a width; it is {}", rect1.width);
+    } else {
+        println!("The rectangle has a no width because it is set to 0");
+    }
+
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+    if rect1.can_hold(&rect2) {
+        println!("yes, rect1 can hold rect2");
+    } else {
+        println!("no, rect1 can not hold rect2");
+    }
+    if rect1.can_hold(&rect3) {
+        println!("yes, rect1 can hold rect3");
+    } else {
+        println!("no, rect1 can not hold rect3");
+    }
 }
