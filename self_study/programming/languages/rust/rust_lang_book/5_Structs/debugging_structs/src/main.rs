8da34b1a134d762033cf580a56210008c3379618
emilbratt <emilbratt@gmail.com>
2023-07-07 16:13:49 +0200

    add method_example

--- self_study/programming/languages/rust/rust_lang_book/5_Structs/debugging_structs/src/main.rs
+++ self_study/programming/languages/rust/rust_lang_book/5_Structs/debugging_structs/src/main.rs
@@ -1,3 +1,27 @@
+// Rust does include functionality to print out debugging information,
+// ..we have to explicitly opt in to make that functionality available.
+// We use derive(debug) decleration.
+#[derive(Debug)]
+// The structs can now be printed out easily, as the one below
+struct Fruits {
+    apples: u32,
+    bananas: u32,
+    oranges: u32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let f = Fruits {
+        apples: 10,
+        bananas: 4,
+        oranges: 8,
+    };
+    // Testing out the printing of the struct in this easy example.
+    println!("The :? examle\n{:?}\n", f);
+    println!("The :#? example\n{:#?}\n", f);
+
+    // We can also use the debug macro provided - also needs the #[derive(Debug)] declaration
+    dbg!(&f);
+
+    // this is an example of how the dbg! macro works - see output from executable
+    dbg!(30 * 30);
 }
