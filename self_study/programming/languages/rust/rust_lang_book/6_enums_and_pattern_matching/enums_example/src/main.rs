5f7ceb109f3a8312565480c3a0c6288aaf4e0c72
emilbratt <emilbratt@gmail.com>
2023-07-13 15:34:52 +0200

    starting with an example for enums

--- self_study/programming/languages/rust/rust_lang_book/6_enums_and_pattern_matching/enums_example/src/main.rs
+++ self_study/programming/languages/rust/rust_lang_book/6_enums_and_pattern_matching/enums_example/src/main.rs
@@ -1,3 +1,29 @@
+#[warn(dead_code)]
+
+
+// IMPLEMENT DATATYPE TO HANDLE THE TWO MAJOR IP VERSIONS
+
+// Declare struct
+struct IpAddr {
+    kind: IpAddrKind, // this is an enum type.. declared below this block and can hold one of two values
+    address: String,
+}
+// .. and add the enum type used in the struct.
+enum IpAddrKind {
+    V4,
+    V6,
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    // Instantiate the IpAddr structs for both IP versions.
+    let local_v4 = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+
+    let local_v6 = IpAddr {
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
 }
