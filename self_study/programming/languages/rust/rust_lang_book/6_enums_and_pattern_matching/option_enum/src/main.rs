e27f3fef748bf3db2ef45cd8aca3645a31d030a5
emilbratt <emilbratt@gmail.com>
2023-07-17 17:02:17 +0200

    option example

--- self_study/programming/languages/rust/rust_lang_book/6_enums_and_pattern_matching/option_enum/src/main.rs
+++ self_study/programming/languages/rust/rust_lang_book/6_enums_and_pattern_matching/option_enum/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+
+    // Going over the Option<T> enum that contains "None" and "Some" fields.
+    let some_number = Some(5); // Gets the type Option<i32>.
+    let some_char = Some('e'); // Gets the type Option<char>.
+    let absent_number: Option<i32> = None; // Gets the type that is annotated Option<i32>, but holds None for now.
+
+    // Some value: we know that a value is present and the value is held within the Some.
+    // None value: in some sense it means the same thing as null (not a valid value).
+
+    // You have to convert an Option<T> to a T before you can perform T operations with it. (T = type).
+    // In general, in order to use an Option<T> value, you want to have code that will handle each variant.
+    // You want some code that will run only when you have a Some(T) value, and this code is allowed to use the inner T.
 }
