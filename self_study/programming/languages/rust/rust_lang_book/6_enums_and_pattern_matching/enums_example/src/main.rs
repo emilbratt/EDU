211698cac2801bf6c34e0d1e3917a85a6baf5f83
emilbratt <emilbratt@gmail.com>
2023-07-18 13:53:20 +0200

    done with chapter 6 enums and patter matching

--- self_study/programming/languages/rust/rust_lang_book/6_enums_and_pattern_matching/enums_example/src/main.rs
+++ self_study/programming/languages/rust/rust_lang_book/6_enums_and_pattern_matching/enums_example/src/main.rs
@@ -1,20 +1,16 @@
-#[warn(dead_code)]
+// Various examples of enums and how they are put into use in Rust.
 
-
-// IMPLEMENT DATATYPE TO HANDLE THE TWO MAJOR IP VERSIONS
-
-// Declare struct
-struct IpAddr {
-    kind: IpAddrKind, // this is an enum type.. declared below this block and can hold one of two values
-    address: String,
-}
-// .. and add the enum type used in the struct.
-enum IpAddrKind {
-    V4,
-    V6,
-}
-
-fn main() {
+fn struct_with_enums() {
+    // Declare a struct
+    struct IpAddr {
+        kind: IpAddrKind, // this is an enum type.. declared below this block and can hold one of two values
+        address: String,
+    }
+    // .. and add an enum for IpAddr.IpAddrKind used in the struct.
+    enum IpAddrKind {
+        V4,
+        V6,
+    }
 
     // Instantiate the IpAddr structs for both IP versions.
     let local_v4 = IpAddr {
@@ -26,4 +22,117 @@ fn main() {
         kind: IpAddrKind::V6,
         address: String::from("::1"),
     };
+}
+
+fn enum_with_enum_pattern() {
+    #[derive(Debug)] // for printing out using {:?}
+
+    enum UsState {
+        Alabama,
+        Alaska,
+        // ..
+    }
+
+    enum Coin {
+        Penny,
+        Nickel,
+        Dime,
+        Quarter(UsState), // Quarters come with designated UsState symbols, we add an enum for this.
+    }
+
+    fn value_in_cents(coin: Coin) -> u8 {
+        match coin {
+            Coin::Penny => {
+                println!("Lucky penny!");
+                1
+            }
+            Coin::Nickel => 5,
+            Coin::Dime => 10,
+            Coin::Quarter(state) => {
+                println!("State quarter from {:?}!", state);
+                25
+            }
+        }
+    }
+
+    value_in_cents(Coin::Penny);
+    value_in_cents(Coin::Nickel);
+    value_in_cents(Coin::Dime);
+    value_in_cents(Coin::Quarter(UsState::Alabama));
+    value_in_cents(Coin::Quarter(UsState::Alaska));
+}
+
+fn catch_all_patterns() {
+
+    fn dice_roll(dice: i8) {
+
+        fn add_fancy_hat() {
+            println!("add fancy hat");
+        }
+
+        fn remove_fancy_hat() {
+            println!("remove fancy hat");
+        }
+
+        fn reroll() {
+            println!("re roll please");
+        }
+
+        match dice {
+            3 => add_fancy_hat(),
+            7 => remove_fancy_hat(),
+            _ => reroll(),
+        }
+    }
+
+    dice_roll(9);
+    dice_roll(7);
+    dice_roll(3);
+}
+
+fn do_nothing_example() {
+    let some_val: i8 = 40;
+    match some_val {
+        20 => println!("some_val is 20"),
+        30 => println!("some_val is 40"),
+        _ => (), // Here, were telling Rust explicitly that we arent going to use any other value.
+                 // E.g. ..we dont want to run any code in this case.
+    }
+}
+
+fn if_let_example() {
+    #[derive(Debug)] // for printing out using {:?}
+
+    // instead of using a match statement, this syntax can shorten the code if only one case has logic.
+    enum Color {
+        Yellow,
+        Blue,
+    }
+
+    enum Car {
+        Toyota,
+        Honda,
+        Porche(Color),
+    }
+
+    fn check_car(car: Car) {
+        if let Car::Porche(clr) = car {
+            println!("The color of the car is {:?}", clr);
+        } else {
+            println!("Car has no colour");
+        }
+    }
+
+    let car_1 = Car::Porche(Color::Yellow);
+    let car_2 = Car::Toyota;
+    check_car(car_1);
+    check_car(car_2);
+}
+
+fn main() {
+    struct_with_enums();
+    enum_with_enum_pattern();
+    catch_all_patterns();
+    do_nothing_example();
+    if_let_example();
 }
--- self_study/programming/languages/rust/rust_lang_book/6_enums_and_pattern_matching/option_enum/src/main.rs
+++ self_study/programming/languages/rust/rust_lang_book/6_enums_and_pattern_matching/option_enum/src/main.rs
@@ -1,9 +1,9 @@
 fn main() {
 
     // Going over the Option<T> enum that contains "None" and "Some" fields.
-    let some_number = Some(5); // Gets the type Option<i32>.
-    let some_char = Some('e'); // Gets the type Option<char>.
-    let absent_number: Option<i32> = None; // Gets the type that is annotated Option<i32>, but holds None for now.
+    let _some_number = Some(5); // Gets the type Option<i32>.
+    let _some_char = Some('e'); // Gets the type Option<char>.
+    let _absent_number: Option<i32> = None; // Gets the type that is annotated Option<i32>, but holds None for now.
 
     // Some value: we know that a value is present and the value is held within the Some.
     // None value: in some sense it means the same thing as null (not a valid value).
@@ -11,4 +11,17 @@ fn main() {
     // You have to convert an Option<T> to a T before you can perform T operations with it. (T = type).
     // In general, in order to use an Option<T> value, you want to have code that will handle each variant.
     // You want some code that will run only when you have a Some(T) value, and this code is allowed to use the inner T.
+
+    // Example using match to handle the option type.
+    let _six = plus_one(Some(5));
+    println!("Result: {:?}", _six);
+    let _none = plus_one(None);
+    println!("Result: {:?}", _none);
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None, // if None -> return None
+        Some(i) => Some(i + 1), // if Some(i) -> return i + 1
+    }
 }
