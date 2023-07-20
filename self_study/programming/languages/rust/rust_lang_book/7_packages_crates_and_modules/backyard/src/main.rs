339f4c02b3f29577ad65677438a4ae3c0455e8af
emilbratt <emilbratt@gmail.com>
2023-07-20 17:05:52 +0200

    cleaning

--- self_study/programming/languages/rust/rust_lang_book/7_packages_crates_and_modules/backyard/src/main.rs
+++ self_study/programming/languages/rust/rust_lang_book/7_packages_crates_and_modules/backyard/src/main.rs
@@ -1,9 +1,11 @@
+// tell compiler to use Asparagus
 use crate::garden::vegetables::Asparagus;
 
-pub mod garden; // ..tells the compiler to include the code it finds in src/garden.rs
+// declare a module
+pub mod garden; // ..the name tells the compiler the module exists in src/garden.rs
 
 fn main() {
-    // We can now use the dummy struct in this example.
+    // We can now use the dummy struct in this example. (see line 1)
     let plant = Asparagus {};
     // Printing is made possible by adding #[derive(Debug)] in the same file as the struct.
     println!("I'm growing {:?}!", plant);
