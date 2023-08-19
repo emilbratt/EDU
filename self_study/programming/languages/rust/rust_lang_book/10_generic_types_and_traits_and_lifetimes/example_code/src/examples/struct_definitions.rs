e3fafc137099311503bd085930673837f9255a53
emilbratt <emilbratt@gmail.com>
2023-08-19 11:43:30 +0200

    learning generic types in struct definitions

--- self_study/programming/languages/rust/rust_lang_book/10_generic_types_and_traits_and_lifetimes/example_code/src/examples/struct_definitions.rs
+++ self_study/programming/languages/rust/rust_lang_book/10_generic_types_and_traits_and_lifetimes/example_code/src/examples/struct_definitions.rs
@@ -1,9 +1,35 @@
-struct Point<T> {
-    x: T,
-    y: T,
+/*
+ * Generic types on Structs
+ *
+ * NOTE: Passing the values to the struct will make it so the generic types
+ *       become the correct primitive type when compiled.
+ *       This is called "type coercion"
+ *       e.g. ..forcing a type based on what the language imply it should be.
+ */
+
+// example when using a struct with a single generic type
+fn same_type_example() {
+    struct SomeStruct<T> {
+        x: T,
+        y: T,
+    }
+
+    let integers = SomeStruct { x: 5, y: 10 };
+    let floats = SomeStruct { x: 1.4, y: 4.3 };
+}
+
+// example when using a struct with multiple generic types
+fn multiple_type_example() {
+    struct SomeStruct<T, U> {
+        x: T,
+        y: U,
+    }
+
+    let float_and_integer = SomeStruct { x: 5.5, y: 10 };
+    let integer_and_float = SomeStruct { x: 5, y: 10.5 };
 }
 
 pub fn run_example() {
-    let integer = Point { x: 5, y: 10 };
-    let float = Point { x: 1.0, y: 4.0 };
+    same_type_example();
+    multiple_type_example();
 }
