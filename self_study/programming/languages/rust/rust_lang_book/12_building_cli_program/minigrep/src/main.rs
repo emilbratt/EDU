9d28684871f4475934faca0efd14efd2c48a9be0
emilbratt <emilbratt@gmail.com>
2023-09-28 14:57:39 +0200

    Finish chapter 12 and our grep implementation "minigrep".

--- self_study/programming/languages/rust/rust_lang_book/12_building_cli_program/minigrep/src/main.rs
+++ self_study/programming/languages/rust/rust_lang_book/12_building_cli_program/minigrep/src/main.rs
@@ -7,15 +7,15 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}"); // print to stderr
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
+    println!("Searching for {}", config.query); // print to stdout
+    println!("In file {}", config.file_path); // print to stdout
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}"); // print to stderr
         process::exit(1);
     }
 }
