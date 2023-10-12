ef623350b5854a65080579be90ac079bbc97f33c
emilbratt <emilbratt@gmail.com>
2023-10-12 14:54:40 +0200

    The example_code directory was created with cargo new.
    Deleted it, create same directory with cargo new --lib.

--- self_study/programming/languages/rust/rust_lang_book/14_More_about_Cargo_and_Crates_dot_io/example_code/src/lib.rs
+++ self_study/programming/languages/rust/rust_lang_book/14_More_about_Cargo_and_Crates_dot_io/example_code/src/lib.rs
@@ -1,8 +1,3 @@
-fn main() {
-    println!("Hello, world!");
-    println!("1 + 1 = {}", add_one(1));
-}
-
 // example where we create documentation for add_one() ..notice the 3 slashes for every line
 /// Adds one to the number given.
 ///
@@ -10,7 +5,7 @@ fn main() {
 ///
 /// ```
 /// let arg = 5;
-/// let answer = my_crate::add_one(arg);
+/// let answer = example_code::add_one(arg);
 ///
 /// assert_eq!(6, answer);
 /// ```
