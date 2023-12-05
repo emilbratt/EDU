dce967a9a6181dea32b7961db1aa825eb70130af
emilbratt <emilbratt@gmail.com>
2023-12-05 09:58:58 +0100

    Adding examples

--- self_study/programming/languages/rust/rust_lang_book/16_fearless_concurrency/example_code/src/move_closure.rs
+++ self_study/programming/languages/rust/rust_lang_book/16_fearless_concurrency/example_code/src/move_closure.rs
@@ -3,9 +3,11 @@ use std::thread;
 pub fn run() {
     let v = vec![1, 2, 3];
 
-    // Moving a value into a new thread with "move", main thread wont use it anymore..
+    // Moving a value into a new thread with "move" => main thread can not use it anymore
     let handle = thread::spawn(move || {
-        println!("Here's a vector: {:?}", v);
+        println!("Hi, from spawned thread");
+        println!("This is the vector I got from the 'move' closure");
+        println!("{:?}", v);
     });
 
     handle.join().unwrap();
