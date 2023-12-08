5def8ab5864f4782a1d62aec607c5e7a5ca3463b
emilbratt <emilbratt@gmail.com>
2023-12-08 19:46:12 +0100

    typing

--- self_study/programming/puzzles/Advent_of_Code/c_sharp/2023/AoC/DayX/Part2.cs
+++ self_study/programming/puzzles/Advent_of_Code/c_sharp/2023/AoC/DayX/Part2.cs
@@ -2,13 +2,9 @@ namespace AoC.DayX;
 
 class Part2
 {
-    #pragma warning disable CS8602 // Dereference of a possibly null reference.
-    private static string? _puzzle_input;
-
     public static string Run(string puzzle_input)
     {
         Console.WriteLine("Running day X part 2");
-        _puzzle_input = puzzle_input;
 
         return String.Empty;
     }
