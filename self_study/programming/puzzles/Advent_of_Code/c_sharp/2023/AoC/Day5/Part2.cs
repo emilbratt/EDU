f315bba7a4a62e73d12e52e159bae773ab6e4174
emilbratt <emilbratt@gmail.com>
2023-12-09 13:15:55 +0100

    AoC Day 5 Part 2
    This solve took	a while	for me to figure out to solve efficiently.
    A brute	force solution worked but took 2 hours of execution.
    I am not satisifed with a brute-force solution.
    
    I was able to implement	a way of avoiding many calculations
    by minimizing the amount of calculations done on input with
    a predictable output.
    
    When starting on the first seed, the next seed to try is increased by N where N=1.
    all the way until the last seed in the range.
    However, if ALSO the output from seed+N increased by N,
    then we can be pretty sure that these fell within the same ranges in all the input output maps.
    E.g seed-to-soil -> soil-to-fertilizer -> fertilizer-to-water..
    
    For eveytime this is the case we increase N by the power of 2.
    N = [1, 2, 4, 8, ..]
    That way we can iterate over the ranges much faster.
    Especially considering ranges in the number of millions.
    
    Summary:
    We increase the seed with +N for everytime f(seed)+N == f(seed+N).
    When this is no longer true we know that somewhere inside the function that seed+N,
    fell within another range.
    When that happens we set N=1 and start over again until the range is completed..

--- self_study/programming/puzzles/Advent_of_Code/c_sharp/2023/AoC/Day5/Part2.cs
+++ self_study/programming/puzzles/Advent_of_Code/c_sharp/2023/AoC/Day5/Part2.cs
@@ -2,13 +2,162 @@ namespace AoC.Day5;
 
 class Part2
 {
-    private static string[]? _puzzle_input;
-
-    public static string Run(string[] puzzle_input)
+    public static string Run(string puzzle_input)
     {
-        Console.WriteLine("Running day 5 part 2");
-        _puzzle_input = puzzle_input;
+        var mapper = new Mapper(puzzle_input);
 
-        return String.Empty;
+        long lowest_location = mapper.FindLowestLocation();
+
+        return lowest_location.ToString();
+    }
+}
+
+class InputHandle
+{
+    public static (long seed, long range)[] GetSeeds(string puzzle_input)
+    {
+        // split all content by new line (and get only the first line)
+        string first_line = puzzle_input.Split("\n")[0];
+
+        // split each part by whitespace
+        string[] whitespace_split = first_line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+        // parse all numbers (skipping first element that holds the string "seeds:")
+        long[] numbers = whitespace_split.Skip(1).Select(long.Parse).ToArray();
+
+        var list_of_tuples = new List<(long, long)>();
+
+        for (int i = 0; i < numbers.Length; i += 2)
+        {
+            (long, long) _t = (numbers[i], numbers[i+1]);
+            list_of_tuples.Add(_t);
+        }
+
+        return list_of_tuples.ToArray();
+    }
+
+    public static (long, long, long)[] GetMaps(string name_of_map, string puzzle_input)
+    {
+        string wanted_input_part = String.Empty;
+
+        foreach (string line in puzzle_input.Split("\n\n").Skip(1))
+        {
+            if (line.Contains(name_of_map)) wanted_input_part = line;
+        }
+
+        if (wanted_input_part == string.Empty) Environment.Exit(1);
+
+        var list_of_tuples = new List<(long, long, long)>();
+        foreach (var numbers in wanted_input_part.Split("\n").Skip(1).ToArray())
+        {
+            if (numbers == "") continue;
+            string[] arr_numbers = numbers.Split(' ');
+            long n1 = long.Parse(arr_numbers[0]);
+            long n2 = long.Parse(arr_numbers[1]);
+            long n3 = long.Parse(arr_numbers[2]);
+            list_of_tuples.Add((n1, n2, n3));
+        }
+
+        return list_of_tuples.ToArray();
+    }
+}
+
+class Mapper
+{
+    private readonly List<(long, long, long)[]> _maps;
+    private readonly (long seed, long range)[] _seeds;
+
+    public Mapper(string puzzle_input)
+    {
+        _seeds = InputHandle.GetSeeds(puzzle_input);
+        _maps = new List<(long, long, long)[]> {
+            InputHandle.GetMaps("seed-to-soil", puzzle_input),
+            InputHandle.GetMaps("soil-to-fertilizer", puzzle_input),
+            InputHandle.GetMaps("fertilizer-to-water", puzzle_input),
+            InputHandle.GetMaps("water-to-light", puzzle_input),
+            InputHandle.GetMaps("light-to-temperature", puzzle_input),
+            InputHandle.GetMaps("temperature-to-humidity", puzzle_input),
+            InputHandle.GetMaps("humidity-to-location", puzzle_input),
+        };
+    }
+
+    public long FindLowestLocation()
+    {
+        var lowest_locations = new List<long>();
+
+        foreach (var seed_tuple in _seeds)
+        {
+            long seed = seed_tuple.seed;
+            long seed_end = seed + seed_tuple.range;
+            long lowest_location = GetLocationFromSeed(seed);
+
+            while (seed < seed_end)
+            {
+                int increment_window = 1;
+
+                long last_seed = seed;
+
+                long result = GetLocationFromSeed(seed);
+                if (result < lowest_location) lowest_location = result;
+
+                long next_result = GetLocationFromSeed(seed + increment_window);
+
+                // uncomment this while block and this will run for a loooong time
+                while (result == next_result - increment_window)
+                {
+                    // this while block is meant to make the solve go faster
+                    // without it the solve will become pure brute-force
+
+                    // for any predicted increasing outcome, we know we can skip calculating it
+                    // this makes it very fast to find the next seed that has an unpredicted value
+
+                    // the predictable outcome
+                    //  f(seed) == f(seed + n) - n
+                    //  ..for all n > seed
+
+                    // since the results are only increasing and we want the lowest value,
+                    // we can omit a max seed limit
+
+                    // for every predictable outcome, increase the increment by the power of 2
+                    increment_window *= 2;
+
+                    result = GetLocationFromSeed(seed);
+                    next_result = GetLocationFromSeed(seed + increment_window);
+
+                    // preserve the last seed for the last correct prediction
+                    last_seed = seed;
+
+                    // increase seed with the increment_window
+                    seed += increment_window;
+                }
+
+                seed = last_seed + 1;
+            }
+
+            lowest_locations.Add(lowest_location);
+        }
+
+        return lowest_locations.Min();
+    }
+
+    private long GetLocationFromSeed(long value)
+    {
+        foreach (var map in _maps)
+        {
+            long offset = GetMapOffset(value, map);
+            value = value + offset;
+        }
+        return value;
+    }
+
+    private static long GetMapOffset(long input, (long dest, long source, long range)[] map)
+    {
+        foreach (var (dest, source, range) in map)
+        {
+            // use map values only if the input falls within the range specified
+            if (input >= source && input < source + range) return dest - source;
+        }
+
+        return 0;
     }
 }
