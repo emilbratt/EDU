dbaf99a22160db17b1ae6efee2a24c0c20fd32af
emilbratt <emilbratt@gmail.com>
2024-02-05 14:00:05 +0100

    AoC 2023 Day 18 Part 2 complete
    This solve would not allow brute-force.
    We solved it using the shoelace formula which gives ut the area of a polygon with point coordinates.
    
    Co-Authored-By: Torje Amundsen <torjefrafen@hotmail.no>

--- self_study/programming/puzzles/Advent_of_Code/c_sharp/2023/AoC/Day18/Part2.cs
+++ self_study/programming/puzzles/Advent_of_Code/c_sharp/2023/AoC/Day18/Part2.cs
@@ -6,9 +6,9 @@ class Part2
     {
         var dig_plan = GetDigPlan(puzzle_input);
 
-        // foreach ((char direction, int meters) in dig_plan) Console.WriteLine($"Dir. '{direction}' | {meters}m.");
+        long res = DigTrench(dig_plan);
 
-        return String.Empty;
+        return res.ToString();
     }
 
     private static (char direction, int meters)[] GetDigPlan(string puzzle_input)
@@ -35,5 +35,50 @@ class Part2
         }
 
         return dig_plan;
+    }
+
+    private static long DigTrench((char direction, int meters)[] dig_plan)
+    {
+        var direction_map = new Dictionary<char, (int row, int col)>()
+        {
+            { 'U', (-1,  0) },
+            { 'D', ( 1,  0) },
+            { 'R', ( 0,  1) },
+            { 'L', ( 0, -1) },
+        };
+
+        long trench_circumference = 0;
+        long trench_area = 0;
+
+        (int x, int y) cur_pos = (0, 0);
+        List<(int x, int y)> coordinates = [];
+
+        foreach ((char direction, int meters) in dig_plan)
+        {
+            var (row, col) = direction_map[direction];
+
+            for (int i = 0; i < meters; i++)
+            {
+                cur_pos.x += col;
+                cur_pos.y += row;
+                trench_circumference++;
+            }
+            coordinates.Add(cur_pos);
+        }
+
+        trench_circumference /= 2;
+
+        for (int i = 1; i < coordinates.Count; i++)
+        {
+            (long x1, long y1) = coordinates[i-1];
+            (long x2, long y2) = coordinates[i];
+
+            trench_area += (x1 * y2) - (x2 * y1);
+        }
+
+        trench_area /= 2;
+        trench_area = Math.Abs(trench_area);
+
+        return trench_area + trench_circumference + 1;
     }
 }
