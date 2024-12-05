67d09078173759a1df01238aa326c59a0a07c775
emilbratt <emilbratt@gmail.com>
2024-12-05 09:17:30 +0100

    AoC 2024 day 5 part 1 complete

--- self_study/programming/puzzles/Advent_of_Code/rust/aoc/src/solutions/y2024d05p1.rs
+++ self_study/programming/puzzles/Advent_of_Code/rust/aoc/src/solutions/y2024d05p1.rs
@@ -1,10 +1,76 @@
 use std::fs;
 
+use std::collections::HashMap;
+
 const INPUT: &str = "y2024d05.in";
 
 pub fn main() {
-    let input: Vec<u8> = fs::read(INPUT).unwrap();
     let input: String = fs::read_to_string(INPUT).unwrap();
 
-    print!("N/A");
+    let mut res: i64 = 0;
+
+    let mut split = input.split("\n\n");
+
+    // page ordering rules
+    let pge_ord_rls = split.next().unwrap();
+
+    // page numbers of each update
+    let pge_nums = split.next().unwrap();
+
+    let pge_ord_rls: HashMap<u8, Vec<u8>> = get_rules(pge_ord_rls);
+
+    for line in pge_nums.lines() {
+        let page_numbers = get_pages(line);
+
+        let mut in_order = true;
+        for i in 1..page_numbers.len() {
+            let (page, next_page) = (page_numbers[i-1], page_numbers[i]);
+
+            if let Some(pages) = pge_ord_rls.get(&page) {
+                if !pages.contains(&next_page) {
+                    in_order = false;
+                    break;
+                }
+            }
+        }
+
+        if in_order {
+            // The middle page number.
+            res += page_numbers[page_numbers.len()/2] as i64;
+        }
+    }
+
+    print!("{res}");
+}
+
+
+fn get_rules(s: &str) -> HashMap<u8, Vec<u8>> {
+    let mut rules: HashMap<u8, Vec<u8>> = HashMap::new();
+
+    for line in s.lines() {
+        let mut split = line.split('|');
+
+        let key = split.next().unwrap().parse::<u8>().unwrap();
+        let value = split.next().unwrap().parse::<u8>().unwrap();
+
+        rules.entry(key)
+            .and_modify(|key| key.push(value))
+            .or_insert(
+                Vec::from([value])
+            );
+    }
+
+    rules
+}
+
+fn get_pages(s: &str) -> Vec<u8> {
+    let mut split = s.split(',');
+
+    let mut update: Vec<u8> = Vec::new();
+
+    while let Some(n) = split.next() {
+        update.push(n.parse::<u8>().unwrap());
+    }
+
+    update
 }
