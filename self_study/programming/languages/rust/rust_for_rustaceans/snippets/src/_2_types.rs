d7020e7b4761baa59eaee55e05fc20dfce600780
emilbratt <emilbratt@gmail.com>
2025-08-21 13:07:13 +0200

    memory layout C

--- self_study/programming/languages/rust/rust_for_rustaceans/snippets/src/_2_types.rs
+++ self_study/programming/languages/rust/rust_for_rustaceans/snippets/src/_2_types.rs
@@ -1,1 +1,15 @@
-pub fn main() {}
+pub fn main() {
+    a();
+}
+
+// Listing 2-1 Same memory layout for types.
+fn a() {
+    #[repr(C)] // Enforce type alignment for compatibility with C.
+    struct Foo {
+        tiny: bool,
+        normal: u32,
+        small: u8,
+        long: u64,
+        short: u16,
+    }
+}
