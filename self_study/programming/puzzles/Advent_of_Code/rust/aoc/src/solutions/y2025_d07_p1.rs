d68fc9846e8b34ec303dcb3fb0796eecf202285e
Emil Bratt <emilbratt@gmail.com>
2025-12-12 18:30:57 +0100

    AoC day 7 part 1 solved

--- self_study/programming/puzzles/Advent_of_Code/rust/aoc/src/solutions/y2025_d07_p1.rs
+++ self_study/programming/puzzles/Advent_of_Code/rust/aoc/src/solutions/y2025_d07_p1.rs
@@ -5,21 +5,8 @@ const INPUT: &str = "y2025_d07.in";
 pub fn main() {
     let input = std::fs::read(INPUT).unwrap();
     let grid = gridify(input);
-    println!();
-
     let ans = solve(grid);
-
-    assert!(
-        ![
-            1725, // to high
-            2017, // to high
-        ].contains(&ans)
-    );
-
-    println!();
     print!("{ans}");
-    println!();
-
 }
 
 fn gridify(input: Vec<u8>) -> Vec<Vec<char>> {
@@ -34,9 +21,6 @@ fn gridify(input: Vec<u8>) -> Vec<Vec<char>> {
 }
 
 fn solve(grid: Vec<Vec<char>>) -> i64 {
-    let mut seen: Vec<(usize, usize, bool)> = Vec::new();
-    let mut done: Vec<(usize, usize)> = Vec::new();
-
     let mut start = 0;
     for i in 0..grid[0].len() {
         if grid[0][i] == 'S' {
@@ -45,30 +29,23 @@ fn solve(grid: Vec<Vec<char>>) -> i64 {
         }
     }
 
-    let mut d: VecDeque<(usize, usize, bool)> = VecDeque::new();
+    let mut ans = 0;
+    let mut visited: Vec<(usize, usize)> = Vec::new();
+    let mut d: VecDeque<(usize, usize)> = VecDeque::new();
+    d.push_back((0, start));
+    while let Some((mut row, col)) = d.pop_back() {
+        while row < grid.len() && !visited.contains(&(row, col)) {
+            visited.push((row, col));
 
-    d.push_back((0, start, true));
-
-    while let Some((mut row, col, right)) = d.pop_back() {
-        while row < grid.len() {
             if grid[row][col] == '^' {
-                if !done.contains(&(row, col+1)) {
-                    d.push_back((row, col+1, true));
-                }
-                if !done.contains(&(row, col-1)) {
-                    d.push_back((row, col-1, false));
-                }
-                done.push((row, col+1));
-                done.push((row, col-1));
-                if !seen.contains(&(row, col, right)) {
-                    seen.push((row, col, right));
-                    row = grid.len();
-                }
+                ans += 1;
+                d.push_back((row, col+1));
+                d.push_back((row, col-1));
+                break;
             }
             row += 1;
         }
-        println!("({row}),({col}) {}", d.len());
     }
 
-    seen.len() as i64
+    ans
 }
