c7b8b6b7f420eedde4b56d5cb2dcf01f6aa94db1
emilbratt <emilbratt@gmail.com>
2025-12-26 21:32:50 +0100

    AoC 2015 day 5 part1

--- self_study/programming/puzzles/Advent_of_Code/rust/aoc/src/solutions/y2015_d05_p1.rs
+++ self_study/programming/puzzles/Advent_of_Code/rust/aoc/src/solutions/y2015_d05_p1.rs
@@ -1,11 +1,39 @@
-#![allow(unused)]
-
 use std::fs;
 
 pub const INPUT: &str = "y2015_d05.in";
 
-pub fn main() {
-    let input: Vec<u8> = fs::read(INPUT).unwrap();
+const VOWELS: [char; 5] = ['a', 'e', 'i', 'o', 'u'];
+const FORBIDDEN: [&str; 4] = ["ab", "cd", "pq", "xy"];
 
-    print!("N/A");
+pub fn main() {
+    let input = fs::read_to_string(INPUT).unwrap();
+
+    let mut ans = 0;
+    'outer: for line in input.lines() {
+        let mut n = 0;
+        let mut twice = false;
+        for s in FORBIDDEN {
+            if line.contains(s) {
+                continue 'outer;
+            }
+        }
+        let bytes = line.as_bytes();
+        for i in 0..bytes.len() {
+            if i < bytes.len() - 1 {
+                if bytes[i] == bytes[i+1] {
+                    twice = true;
+                }
+            }
+            if VOWELS.contains(&(bytes[i] as char)) {
+                n += 1;
+            }
+            if n >= 3 && twice {
+                ans += 1;
+                continue 'outer;
+            }
+        }
+    }
+
+
+    print!("{ans}");
 }
