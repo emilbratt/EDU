b12075016764ea6261d0f92a8d70a70527b2913d
emilbratt <emilbratt@gmail.com>
2025-12-30 20:24:08 +0100

    AoC 2015 day 8 part 2 (shortest solve yet?)

--- self_study/programming/puzzles/Advent_of_Code/rust/aoc/src/solutions/y2015_d08_p2.rs
+++ self_study/programming/puzzles/Advent_of_Code/rust/aoc/src/solutions/y2015_d08_p2.rs
@@ -1,7 +1,12 @@
 pub fn main() {
-    let input: Vec<u8> = std::fs::read("y2015_d08.in").unwrap();
-
-    let mut ans: i64 = 0;
-
+    // Lets solve this in one go!
+    let ans = std::fs::read_to_string("y2015_d08.in")
+        .unwrap()
+        .lines()
+        .fold(0, |acc, line| {
+            // Exploting the debug functionality using format!(),
+            // this will expand the string for. :):)
+            acc + format!("{:?}", line).len() - line.len()
+        });
     print!("{ans}");
 }
