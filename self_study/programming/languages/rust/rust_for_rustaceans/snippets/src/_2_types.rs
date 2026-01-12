c0b6c129e776c0a29e0688a5b357f9a4cdc645f4
emilbratt <emilbratt@gmail.com>
2026-01-12 12:10:03 +0100

    typing

--- self_study/programming/languages/rust/rust_for_rustaceans/snippets/src/_2_types.rs
+++ self_study/programming/languages/rust/rust_for_rustaceans/snippets/src/_2_types.rs
@@ -1,15 +1,25 @@
 pub fn main() {
-    a();
+    let bar = Bar { a: 1, b: String::from("hello") };
+    println!("{}", bar.b);
 }
 
-// Listing 2-1 Same memory layout for types.
-fn a() {
-    #[repr(C)] // Enforce type alignment for compatibility with C.
-    struct Foo {
-        tiny: bool,
-        normal: u32,
-        small: u8,
-        long: u64,
-        short: u16,
-    }
+// Type alignmnet in memory..
+
+// Enforce type alignment for compatibility with C. This will introduce padding of bytes.
+#[repr(C)]
+struct Foo {
+    tiny: bool, // bool will byte align as it is the first value..
+    normal: u32, // 4 byte type -> compiler inserts 3 bytes of padding after tiny's 1 byte, to align correctly..
+    small: u8, // and so one..
+    long: u64, // ..
+    short: u16, // ..
 }
+
+// Option for larger alignment (can improve performance when store in for example an array)
+#[repr(align(2))]
+struct Bar {
+    a: u32,
+    b: String,
+}
+// Common use case to ensure different values stored contigously in memory
+// end up in different cache lines on the CPU. Can improve performance.
