1f574d1902b01ceef4584a88393ae84a71ee6d2e
emilbratt <emilbratt@gmail.com>
2026-01-12 20:08:35 +0100

    typing

--- self_study/programming/languages/rust/rust_for_rustaceans/snippets/src/_2_types.rs
+++ self_study/programming/languages/rust/rust_for_rustaceans/snippets/src/_2_types.rs
@@ -4,7 +4,6 @@ pub fn main() {
 }
 
 // Type alignmnet in memory..
-
 // Enforce type alignment for compatibility with C. This will introduce padding of bytes.
 #[repr(C)]
 struct Foo {
@@ -23,3 +22,8 @@ struct Bar {
 }
 // Common use case to ensure different values stored contigously in memory
 // end up in different cache lines on the CPU. Can improve performance.
+
+
+// VTable
+// Every trait object holds a vtable. A vtable is a dispatch table
+// which during run-time, refers to the correct function for that specific type.
