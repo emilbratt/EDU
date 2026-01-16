24c98cff6788219c5547ed91a8ff05bb45e43883
emilbratt <emilbratt@gmail.com>
2026-01-16 10:40:43 +0100

    typing

--- self_study/programming/projects/png_decoder/src/lib.rs
+++ self_study/programming/projects/png_decoder/src/lib.rs
@@ -13,14 +13,32 @@ struct Chunk {
     data: Vec<u8>,
 }
 
+// just to print out the hex values..
+fn hex(bytes: &[u8]) -> String {
+    let mut s = String::with_capacity(bytes.len());
+    for b in bytes {
+        let fmt = format!("{:X}", b);
+        s.push_str( format!("{:0>2} ", fmt).as_str() );
+    }
+    println!("{}", s);
+    s
+}
+
 pub fn load() -> io::Result<()> {
-    let mut buf = [0u8; 8];
+    let mut signature_buf = [0u8; 8];
+    let mut chunk_buf = [0u8; 4];
     let f = File::open("images/2x2.png").unwrap();
     let mut rdr = BufReader::new(f);
-    let n = rdr.read(&mut buf)?;
+    let n = rdr.read(&mut signature_buf)?;
     assert_eq!(n, 8);
-    assert_eq!(PNG_SIGNATURE, buf);
-    println!("{:?}", buf);
+    assert_eq!(PNG_SIGNATURE, signature_buf);
+    hex(&signature_buf);
+
+    while let Ok(n) = rdr.read(&mut chunk_buf) {
+        hex(&chunk_buf);
+        break;
+    }
+
     Ok(())
 }
 
